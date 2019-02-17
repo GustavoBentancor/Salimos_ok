bc9e7d2be70c57a006054ff2d5c76b2bc327d6bd
GustavoBentancor <43424913+GustavoBentancor@users.noreply.github.com>
2019-02-17 10:55:42 -0300

    Update urls.py

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.urls import include
 
 urlpatterns = [
     
-    path('', views.inicio, name='inicio')
+   path('', views.inicio, name='inicio'),
    path('admin/', admin.site.urls),
     ]
